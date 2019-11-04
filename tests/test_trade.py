90ad0cbe6e0c451af36bbbf2ed45641171f221b9
acrainier1 <alex.c.canizales@gmail.com>
2019-11-04 15:57:29 -0500

    terminal trader

--- tests/test_trade.py
+++ tests/test_trade.py
@@ -12,5 +12,31 @@ class TestTrade(unittest.TestCase):
         schema(DBPATH)
         setDB(DBPATH)
 
+
+    @classmethod
+    def tearDownClass(cls):
+        # called once when all the tests in this file are done
+        # os.remove(DBPATH)
+        pass
+
+
+    def setUp(self):
+        # called once before EACH test method
+        with sqlite3.connect(Account.dbpath) as conn:
+            cur = conn.cursor()
+            DELETESQL = "DELETE FROM trades;"
+            cur.execute(DELETESQL)
+            #joe = Account(first="Joe", last="Smith", account_num="00000009", balance=100.0, pin="0009")
+            #joe.save()
+            #self.joe_id = joe.id
+
+
+    def tearDown(self):
+        # called once after EACH test method
+        pass
+
     def test_dummy(self):
         self.assertTrue(True)
+
+
+    
