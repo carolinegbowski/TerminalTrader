912052dade926ffba623831e01cfb5a8dfde732f
Caroline <carolinegrabowski@Carolines-MacBook-Pro.local>
2019-11-06 10:57:44 -0500

    completed positions & 1/2 way through tests

--- schema.py
+++ schema.py
@@ -1,8 +1,8 @@
 import sqlite3 
 from app.config import DBPATH 
 
-def schema(dbpath=DBPATH):
-    """
+
+"""
     Create these tables
 
     account:
@@ -14,7 +14,57 @@ def schema(dbpath=DBPATH):
     trade:
         id, ticker, volume, time, price, account_id
     """
-    pass
+
+
+def schema(dbpath=DBPATH):
+    CREATE_SQL_ACCOUNTS = """
+
+    CREATE TABLE accounts(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username VARCHAR(15) NOT NULL,
+        password_hash VARCHAR(30),
+        balance FLOAT
+        first_name VARCHAR(200),
+        last_name VARCHAR(200),
+        email_address VARCHAR(50)
+    ); """
+
+    CREATE_SQL_POSITIONS = """
+    CREATE TABLE positions(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        ticker VARCHAR(15) NOT NULL,
+        shares FLOAT,
+        account_id INTEGER,
+        FOREIGN KEY ("account_id") REFERENCES accounts(id)
+    ); """
+    # UNIQUE(ticker, account_id)
+    
+    CREATE_SQL_TRADES = """
+    CREATE TABLE trades(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        ticker VARCHAR(5) NOT NULL,
+        volume FLOAT,
+        unit_price FLOAT,
+        time DATE,
+        account_id INTEGER,
+        FOREIGN KEY ("account_id") REFERENCES accounts(id)
+    );
+        """
+
+    DROPSQL_ACCOUNTS = "DROP TABLE IF EXISTS accounts;"
+    DROPSQL_POSITIONS = "DROP TABLE IF EXISTS positions;"
+    DROPSQL_TRADES = "DROP TABLE IF EXISTS trades;"
+
+
+    with sqlite3.connect(dbpath) as conn:
+        cursor = conn.cursor()
+        cursor.execute(DROPSQL_ACCOUNTS)
+        cursor.execute(DROPSQL_POSITIONS)
+        cursor.execute(DROPSQL_TRADES)
+        cursor.execute(CREATE_SQL_ACCOUNTS)
+        cursor.execute(CREATE_SQL_POSITIONS)
+        cursor.execute(CREATE_SQL_TRADES)
+
 
 if __name__ == "__main__":
     schema()
--- tests/test_position.py
+++ tests/test_position.py
@@ -1,4 +1,5 @@
 import unittest
+import sqlite3 
 from app import Account, Trade, Position, setDB
 from app import InsufficientFundsError, InsufficientSharesError, NoSuchTickerError
 from schema import schema
@@ -11,6 +12,88 @@ class TestPosition(unittest.TestCase):
     def setUpClass(cls):
         schema(DBPATH)
         setDB(DBPATH)
+    
+    @classmethod
+    def tearDownClass(cls):
+        # called once when all the tests in this file are done
+        # os.remove(DBPATH)
+        pass
+
+    def setUp(self):
+        with sqlite3.connect(DBPATH) as connection:
+            cursor = connection.cursor()
+            DELETESQL = "DELETE FROM positions;"
+            cursor.execute(DELETESQL)
+            # apple = Position(ticker="AAPL", shares=100, account_id=3)
+            # apple.save()
+            # self.apple_id = apple.id
+    
+    def tearDown(self):
+        pass
 
     def test_dummy(self):
         self.assertTrue(True)
+    
+    def testSaveInsert(self):
+        apple = Position(ticker="AAPL", shares=100, account_id=3)
+        apple.save()
+        self.assertIsNotNone(apple.id, "save should set an id value for new input")
+        with sqlite3.connect(DBPATH) as connection:
+            cursor = connection.cursor()
+            SQL = "SELECT * FROM positions WHERE shares=100;"
+            cursor.execute(SQL)
+            rows = cursor.fetchall()
+            self.assertEqual(len(rows), 1, "save should create 1 new row in the database")
+        
+    def testSaveUpdate(self):
+        apple = Position(ticker="AAPL", shares=100, account_id=3)
+        apple.save()
+        apple_id = apple.id
+        apple2 = Position.from_id(apple_id)
+        apple2.shares = 150
+        apple2.save()
+        self.assertEqual(apple2.id, apple_id, "update should not change ID number")
+
+        apple3 = Position.from_id(apple_id)
+        self.assertEqual(apple3.ticker,  "AAPL", "save update should keep same ticker if not changed")
+        self.assertEqual(apple3.shares,  150, "save updates should increase share amt by 50")
+        self.assertEqual(apple3.account_id,  3, "save should keep the same account_id if not changed")
+
+    def testDelete(self):
+        apple = Position(ticker="AAPL", shares=100, account_id=3)
+        apple.save()
+        orange = Position(ticker="ORNG", shares=150, account_id=3)
+        orange.save()
+        apple.delete()
+        with sqlite3.connect(DBPATH) as connection:
+            cursor = connection.cursor()
+            SQL = "SELECT * FROM positions;"
+            cursor.execute(SQL)
+            rows = cursor.fetchall()
+            self.assertEqual(len(rows), 1, "delete should delete 1 row in the database")
+    
+    def testDeleteAll(self):
+        apple = Position(ticker="AAPL", shares=100, account_id=3)
+        apple.save()
+        orange = Position(ticker="ORNG", shares=150, account_id=3)
+        orange.save()
+        Position.delete_all()
+        with sqlite3.connect(DBPATH) as connection:
+            cursor = connection.cursor()
+            SQL = "SELECT * FROM positions;"
+            cursor.execute(SQL)
+            rows = cursor.fetchall()
+            self.assertEqual(len(rows), 0, "delete_all should delete all rows in the database")
+    
+    def testFromId(self):
+        apple = Position(ticker="AAPL", shares=100, account_id=3)
+        apple.save()
+        apple_id = apple.id
+        apple2 = Position.from_id(apple_id)
+        self.assertEqual(apple2.ticker, "AAPL", "Data should be loaded from database with .from_id()")
+        orange = Position.from_id(100000000000)
+        self.assertIsNone(orange, ".from_id() returns None for nonexistent row")
+
+
+
+
